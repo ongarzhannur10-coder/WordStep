571035c2592031a5b8cc626c69fd21d0e576938c
ongarzhannur10-coder <ongarzhannur10@gmail.com>
2026-03-14 10:54:07 +0500

    Create User model with password hashing and comparison

---
+++ server/models/server/models/User.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  level: {
+    type: String,
+    enum: ['A1', 'A2', 'B1'],
+    default: 'A1'
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  badges: {
+    type: [String],
+    default: []
+  },
+  completedLessons: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: []
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
